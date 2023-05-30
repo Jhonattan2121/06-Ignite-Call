43a1efae743f9443e5c699a50306308e09dd6b4b
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-05-30 17:42:57 -0300

    Manipulando cookies no Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,7 @@
 
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,14 +11,33 @@ export default async function handler(
         return res.status(405).end()
     }
 
+    
+
     const {name, username} = req.body
 
-    const User = await prisma.user.create({
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username,
+        },
+    })
+
+    if (userExists) {
+        return res.status(400).json({
+            message: 'Username already taken.'
+        })
+    }
+
+    const user = await prisma.user.create({
         data: {
             name,
             username,
         }
     })
 
-  return  res.status(201).json(User)
+    setCookie({ res, }, '@Ignite-Call:userId', user.id, {
+        maxAge: 60 * 60 * 24 * 7, // 7 days 
+        path: '/',
+    })
+
+  return  res.status(201).json(user)
 }
