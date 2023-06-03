fe9ac71f67e02e2ae5fc94b13ddf492f20de015f
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-06-02 23:53:53 -0300

    Estrutura visual do calendário

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { Avatar, Heading, Text } from "@ignite-ui/react";
 import { Container, UserHeader } from "./styles";
 import { GetStaticProps, GetStaticPaths } from "next";
 import prisma from "@/libs/prisma";
+import { ScheduleForm } from "./ScheduleForm";
 
 interface ScheduleProps {
   user: {
@@ -20,6 +21,7 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{name}</Heading>
         <Text>{bio}</Text>
       </UserHeader>
+      <ScheduleForm />
     </Container>
   );
 }
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,19 +1,19 @@
-import { daysInWeek } from "date-fns";
 
-export function getWeekDays() {
-    const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' });
-  
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
-    const startDay = daysInWeek; 
-  
-    const weekDays = Array.from(Array(7).keys())
-      .map((day) => {
-        const date = new Date(currentYear, currentMonth, startDay + day);
-        return formatter.format(date);
-      });
-  
-    return weekDays;
+
+interface GetWeekDaysParams {
+  short?: boolean;
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
+    const formatter = new Intl.DateTimeFormat('pt-BR', {weekday: 'long'})
+
+    return Array.from(Array(7).keys()).map((day) => formatter.format((new Date(Date.UTC(2023, 5, day)))))
+    .map((weekDay) => {
+      if (short) {
+        return weekDay.slice(0, 3)
+      }
+      return weekDay.substring(0, 1 ).toUpperCase().concat(weekDay.substring(1))
+    })
+       
   }
   
