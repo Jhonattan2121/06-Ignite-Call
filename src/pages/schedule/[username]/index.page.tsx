8ba93d9fbef2eab2884bb822558f639fcbd62f68
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-06-02 22:52:15 -0300

    Página de agendamento

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,65 @@
+import { Avatar, Heading, Text } from "@ignite-ui/react";
+import { Container, UserHeader } from "./styles";
+import { GetStaticProps, GetStaticPaths } from "next";
+import prisma from "@/libs/prisma";
+
+interface ScheduleProps {
+  user: {
+    name: string;
+    bio: string;
+    avatarUrl: string;
+  };
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  const { name, bio, avatarUrl } = user;
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={avatarUrl} />
+        <Heading>{name}</Heading>
+        <Text>{bio}</Text>
+      </UserHeader>
+    </Container>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const users = await prisma.user.findMany();
+
+  const paths = users.map((user) => ({
+    params: { username: user.username },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60  * 24, // 1 Day
+};
+};
