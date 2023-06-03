e0a87a56135e9a40deca99bec923449ca65b5df7
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-06-03 00:31:50 -0300

    Estilização do calendário

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -4,7 +4,7 @@ interface GetWeekDaysParams {
   short?: boolean;
 }
 
-export function getWeekDays({ short = false }: GetWeekDaysParams) {
+export function getWeekDays({ short = false }: GetWeekDaysParams ) {
     const formatter = new Intl.DateTimeFormat('pt-BR', {weekday: 'long'})
 
     return Array.from(Array(7).keys()).map((day) => formatter.format((new Date(Date.UTC(2023, 5, day)))))
